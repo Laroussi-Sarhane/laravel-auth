970b2cf1ddc303ac65950dba7727e45f6543cfa1
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-24 09:32:32 +0200

    add

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,12 +1,12 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+        <div class="container-fluid d-flex  justify-content-between ">
+          <a class="navbar-brand" href="#"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <div class="collapse navbar-collapse d-flex  justify-content-between " id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0  ">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="{{route('admin.home')}}">Home</a>
               </li>
@@ -17,7 +17,11 @@
                 <p> {{Auth::user()->name}} </p>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-disabled="true">logout</a>
+                <form action="{{route('logout')}} " method="POST">
+                    @csrf
+                    <button type="submit" class=" btn  btn-danger"></button>
+                    <a class="nav-link" aria-disabled="true">logout</a>
+                </form>
               </li>
 
             </ul>
